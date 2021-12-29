adf6497bec5154e9345bfb1658770bf8a77db738
Dwi <98dwiyulianto@gmail.com>
2021-12-29 17:45:11 +0700

    Improve code to method chaining 2

--- application/models/Berita_model.php
+++ application/models/Berita_model.php
@@ -20,70 +20,66 @@ class Berita_model extends CI_Model {
 
 	// Listing author admin
 	public function author_admin($id_user) {
-		$this->db->select('berita.*, users.nama, nama_kategori');
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		$this->db->join('kategori_berita','kategori_berita.id_kategori_berita = berita.id_kategori_berita','LEFT');
-		$this->db->join('users','users.id_user = berita.id_user','LEFT');
+		return $this->db->select('berita.*, 
+											users.nama,
+											nama_kategori')
+						->from('berita')
+						->join('kategori_berita','kategori_berita.id_kategori_berita = berita.id_kategori_berita','LEFT')
+						->join('users','users.id_user = berita.id_user','LEFT')
+						->where(array(	'berita.id_user'	=> $id_user))
+						->order_by('id_berita','DESC')
+						->get()->result();
 		// End join
-		$this->db->where(array(	'berita.id_user'	=> $id_user));
-		$this->db->order_by('id_berita','DESC');
-		$query = $this->db->get();
-		return $query->result();
 	}
 
 	// Listing kategori admin
 	public function kategori_admin($id_kategori) {
-		$this->db->select('berita.*, users.nama, kategori_berita.nama_kategori');
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		
-		$this->db->join('users','users.id_user = berita.id_user','LEFT');
-		$this->db->join('kategori_berita','kategori_berita.id_kategori_berita = berita.id_kategori_berita','LEFT');
+		return $this->db->select('berita.*, users.nama, kategori_berita.nama_kategori')
+						->from('berita')
+						->join('users','users.id_user = berita.id_user','LEFT')
+						->join('kategori_berita','kategori_berita.id_kategori_berita = berita.id_kategori_berita','LEFT')
+						->where(array(	'berita.id_kategori_berita'	=> $id_kategori))
+						->order_by('id_berita','DESC')
+						->get()->result();
 		// End join
-		$this->db->where(array(	'berita.id_kategori_berita'	=> $id_kategori));
-		$this->db->order_by('id_berita','DESC');
-		$query = $this->db->get();
-		return $query->result();
 	}
 
 	// Listing jenis admin
 	public function jenis_admin($jenis_berita) {
-		$this->db->select('berita.*, users.nama, kategori_berita.nama_kategori_berita, kategori_berita.slug_kategori');
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		$this->db->join('kategori_berita','kategori_berita.id_kategori_berita = berita.id_kategori_berita','LEFT');
-		$this->db->join('users','users.id_user = berita.id_user','LEFT');
+		return $this->db->select('berita.*, users.nama, kategori_berita.nama_kategori_berita, kategori_berita.slug_kategori')
+						->from('berita')
+						->join('kategori_berita','kategori_berita.id_kategori_berita = berita.id_kategori_berita','LEFT')
+						->join('users','users.id_user = berita.id_user','LEFT')
+						->where(array(	'berita.jenis_berita'	=> $jenis_berita))
+						->order_by('id_berita','DESC')
+						->get()->result();
 		// End join
-		$this->db->where(array(	'berita.jenis_berita'	=> $jenis_berita));
-		$this->db->order_by('id_berita','DESC');
-		$query = $this->db->get();
-		return $query->result();
 	}
 	
 	// Read berita main page
 	public function read($slug_berita)
 	{
-		$this->db->select('berita.*, kategori_berita.nama_kategori, kategori_berita.slug_kategori, users.nama');
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		$this->db->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left');
-		$this->db->join('users', 'users.id_user = berita.id_user', 'left');
-		$this->db->where(['berita.status_berita' => 'publish', 'berita.slug_berita' => $slug_berita]);	
-		$this->db->order_by('id_berita', 'DESC');
-		$query = $this->db->get();
-		return $query->row();
+		return $this->db->select('berita.*, kategori_berita.nama_kategori, kategori_berita.slug_kategori, users.nama')
+						->from('berita')
+						->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left')
+						->join('users', 'users.id_user = berita.id_user', 'left')
+						->where(['berita.status_berita' => 'publish', 'berita.slug_berita' => $slug_berita])
+						->order_by('id_berita', 'DESC')
+						->get()->row();
 	} 
 
 	// Detail Berita
 	public function detail($id_berita)
 	{
-		$this->db->select('*');
-		$this->db->from('berita');
-		$this->db->where('id_berita', $id_berita);
-		$this->db->order_by('id_berita');
-		$query = $this->db->get();
-		return $query->row();
+		return $this->db->select('*')
+						->from('berita')
+						->where('id_berita', $id_berita)
+						->order_by('id_berita')
+						->get()->row();
 	}
 
 	// Tambah Data
@@ -118,8 +114,8 @@ class Berita_model extends CI_Model {
 	// Delete
 	public function hapus($id_berita)
 	{
-		$this->db->where('id_berita', $id_berita);
-		return $this->db->delete('berita');
+		return $this->db->where('id_berita', $id_berita)
+						->delete('berita');
 	}
 
 	// Main Page
@@ -127,103 +123,96 @@ class Berita_model extends CI_Model {
 	
 	// Edit hit
 	public function update_hit($hit) {
-		$this->db->where('id_berita',$hit['id_berita']);
-		$this->db->update('berita',$hit);
+		$this->db->where('id_berita',$hit['id_berita'])
+				 ->update('berita',$hit);
 	}
 	
 	public function home()
 	{
-		$this->db->select('berita.*, kategori_berita.nama_kategori, kategori_berita.slug_kategori, users.nama');
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		$this->db->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left');
-		$this->db->join('users', 'users.id_user = berita.id_user', 'left');
-		$this->db->where(['status_berita' => 'publish', 'jenis_berita' => 'berita']);	
-		
-		$this->db->order_by('id_berita', 'DESC');
-		$this->db->limit(5);
-		$query = $this->db->get();
-		return $query->result();
+		return $this->db->select('berita.*, 
+											kategori_berita.nama_kategori, kategori_berita.slug_kategori, 
+											users.nama')
+						->from('berita')
+						->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left')
+						->join('users', 'users.id_user = berita.id_user', 'left')
+						->where(['status_berita' => 'publish', 'jenis_berita' => 'berita'])
+						->order_by('id_berita', 'DESC')
+						->limit(5)
+						->get()->result();
 	} 
 	
 	// Berita terbaru
 	public function berita_baru()
 	{
-		$this->db->select('berita.*, kategori_berita.nama_kategori, kategori_berita.slug_kategori, users.nama');
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		$this->db->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left');
-		$this->db->join('users', 'users.id_user = berita.id_user', 'left');
-		$this->db->where(['status_berita' => 'publish', 'jenis_berita' => 'berita']);	
-		
-		$this->db->order_by('id_berita', 'DESC');
-		$this->db->limit(9);
-		$query = $this->db->get();
-		return $query->result();
+		return $this->db->select('berita.*, kategori_berita.nama_kategori, kategori_berita.slug_kategori, users.nama')
+					->from('berita')
+					->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left')
+					->join('users', 'users.id_user = berita.id_user', 'left')
+					->where(['status_berita' => 'publish', 'jenis_berita' => 'berita'])
+					->order_by('id_berita', 'DESC')
+					->limit(9)
+					->get()->result();
 	} 
 
 	// Listing main page
 	public function berita($limit, $start)
 	{
-		$this->db->select("berita.*, DATE_FORMAT(berita.tanggal_post, '%W %M %Y') AS tanggalPost,
-											kategori_berita.nama_kategori, kategori_berita.slug_kategori, 
-											users.nama");
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		$this->db->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left');
-		$this->db->join('users', 'users.id_user = berita.id_user', 'left');
-		$this->db->where(['status_berita' => 'publish', 'berita.jenis_berita' => 'berita']);	
-		
-		$this->db->order_by('id_berita', 'DESC');
-		$this->db->limit($limit, $start);
-		$query = $this->db->get();
-		return $query->result();
+		return $this->db->select("berita.*, DATE_FORMAT(berita.tanggal_post, '%W %M %Y') AS tanggalPost,
+											kategori_berita.nama_kategori, kategori_berita.slug_kategori, 
+											users.nama")
+						->from('berita')
+						->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left')
+						->join('users', 'users.id_user = berita.id_user', 'left')
+						->where(['status_berita' => 'publish', 'berita.jenis_berita' => 'berita'])
+						->order_by('id_berita', 'DESC')
+						->limit($limit, $start)
+						->get()->result();
 	} 
 
 	// Listing main page
 	public function total_berita()
 	{
-		$this->db->select('berita.*, kategori_berita.nama_kategori, kategori_berita.slug_kategori, users.nama');
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		$this->db->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left');
-		$this->db->join('users', 'users.id_user = berita.id_user', 'left');
-		$this->db->where(['status_berita' => 'publish']);	
-		
-		$this->db->order_by('id_berita', 'DESC');
-		$query = $this->db->get();
-		return $query->result();
+		return $this->db->select('berita.*, kategori_berita.nama_kategori, kategori_berita.slug_kategori, users.nama')
+					->from('berita')
+					->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left')
+					->join('users', 'users.id_user = berita.id_user', 'left')
+					->where(['status_berita' => 'publish'])
+					->order_by('id_berita', 'DESC')
+					->get()->result();
 	}
 	
 	// Listing main kategori berita
 	public function kategori_berita($id_kategori, $limit, $start)
 	{
-		$this->db->select("berita.*, DATE_FORMAT(berita.tanggal_post, '%W %M %Y') AS tanggalPost,
-														kategori_berita.nama_kategori, kategori_berita.slug_kategori, 
-														users.nama");
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		$this->db->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left');
-		$this->db->join('users', 'users.id_user = berita.id_user', 'left');
-		$this->db->where(['status_berita' => 'publish', 'berita.id_kategori_berita' => $id_kategori]);	
-		$this->db->order_by('id_berita', 'DESC');
-		$this->db->limit($limit, $start);
-		$query = $this->db->get();
-		return $query->result();
+		return $this->db->select("berita.*, DATE_FORMAT(berita.tanggal_post, '%W %M %Y') AS tanggalPost,
+											 kategori_berita.nama_kategori, kategori_berita.slug_kategori, 
+											 users.nama")
+						->from('berita')
+						->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left')
+						->join('users', 'users.id_user = berita.id_user', 'left')
+						->where(['status_berita' => 'publish', 'berita.id_kategori_berita' => $id_kategori])
+						->order_by('id_berita', 'DESC')
+						->limit($limit, $start)
+						->get()->result();
 	} 
 
 	// Listing main kategori berita
 	public function total_kategori_berita($id_kategori)
 	{
-		$this->db->select('berita.*, kategori_berita.nama_kategori, kategori_berita.slug_kategori, users.nama');
-		$this->db->from('berita');
 		// Join dg 2 tabel
-		$this->db->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left');
-		$this->db->join('users', 'users.id_user = berita.id_user', 'left');
-		$this->db->where(['status_berita' => 'publish', 'berita.id_kategori_berita' => $id_kategori]);	
-		$this->db->order_by('id_berita', 'DESC');
-		$query = $this->db->get();
-		return $query->result();
+		return $this->db->select('berita.*, kategori_berita.nama_kategori, kategori_berita.slug_kategori, 
+											users.nama')
+						->from('berita')
+						->join('kategori_berita', 'kategori_berita.id_kategori_berita = berita.id_kategori_berita', 'left')
+						->join('users', 'users.id_user = berita.id_user', 'left')
+						->where(['status_berita' => 'publish', 'berita.id_kategori_berita' => $id_kategori])
+						->order_by('id_berita', 'DESC')
+						->get()->result();
 	} 
 }
 
